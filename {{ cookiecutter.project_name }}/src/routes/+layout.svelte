c5f8db6e33ffdac7ac151c147819229b70fa401e
PerchunPak <git@perchun.it>
2023-09-10 03:31:46 +0200

    Fix putting space in incorrect place if not centering

--- {{ cookiecutter.project_name }}/src/routes/+layout.svelte
+++ {{ cookiecutter.project_name }}/src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 	import '../app.css';
 </script>
 
-<main class="{% if cookiecutter.center_everything == 'y' -%}flex flex-col items-center justify-center{% endif %} m-0 p-5 w-full min-h-screen">
+<main class="{% if cookiecutter.center_everything == 'y' -%}flex flex-col items-center justify-center {% endif %}m-0 p-5 w-full min-h-screen">
 	<slot />
 </main>
